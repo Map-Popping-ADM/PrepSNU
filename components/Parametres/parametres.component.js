7cc427bb03bc742bc2c5dbdfbb99160294053f5b
Lea Lamzani <leo.cholvy@gmail.com>
2022-03-31 19:00:43 +0200

    toggle mode sombre/clair

--- components/Parametres/parametres.component.js
+++ components/Parametres/parametres.component.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { SafeAreaView } from 'react-native';
-import { Divider, Icon, Layout, Text, TopNavigation, TopNavigationAction } from '@ui-kitten/components';
+import { Toggle, Divider, Icon, Layout, Text, TopNavigation, TopNavigationAction } from '@ui-kitten/components';
 
 const BackIcon = (props) => (
   <Icon {...props} name='arrow-back' />
@@ -16,12 +16,32 @@ export const ParametreScreen = ({ navigation }) => {
     <TopNavigationAction icon={BackIcon} onPress={navigateBack}/>
   );
 
+  const [checked, setChecked] = React.useState(true);
+  const [messageMode, setMessageMode] = React.useState("Mode clair");
+
+  const onCheckedChange = (isChecked) => {
+    setChecked(isChecked);
+
+    if (checked) {
+      setMessageMode ("Mode sombre");
+    } else {
+      setMessageMode ("Mode clair");
+    }
+  };
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <TopNavigation title='MyApp' alignment='center' accessoryLeft={BackAction} subtitle='parametres'/>
       <Divider/>
-      <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text category='h1'>Settings</Text>
+      <Layout style={{ flex: 1, flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'flex-start'}}>
+        <Layout style={{ flex: 1, flexDirection: 'row', justifyContent: 'flex-start', alignItems: 'center'}}>
+        <Toggle checked={checked} onChange={onCheckedChange} style={{padding:15}}>
+          <Text>
+            { messageMode }
+          </Text>
+        </Toggle>
+        </Layout>
+        <Divider/>
       </Layout>
     </SafeAreaView>
   );
